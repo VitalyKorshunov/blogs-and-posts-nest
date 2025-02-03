9cbddeaf7a1d957be76e54006cf8c1943d7c8656
VitalyKorshunov <vitaliy.korshunov1994@gmail.com>
2025-02-03 19:58:37 +0300

    refactor(tests): improve database cleanup logic in testing module

--- src/features/testing/testing.controller.ts
+++ src/features/testing/testing.controller.ts
@@ -1,40 +1,25 @@
 import { Controller, Delete, HttpCode, HttpStatus } from '@nestjs/common';
-import { InjectModel } from '@nestjs/mongoose';
-import { User, UserModelType } from '../user-accounts/domain/user.entity';
-import {
-  Blog,
-  BlogModelType,
-} from '../blogger-platform/blogs/domain/blog.entity';
-import {
-  Like,
-  LikeModelType,
-} from '../blogger-platform/likes/domain/like.entity';
-import {
-  Comment,
-  CommentModelType,
-} from '../blogger-platform/comments/domain/comment.entity';
-import {
-  Post,
-  PostModelType,
-} from '../blogger-platform/posts/domain/post.entity';
+import { InjectConnection } from '@nestjs/mongoose';
+import { Connection } from 'mongoose';
 
 @Controller('testing')
 export class TestingController {
   constructor(
-    @InjectModel(User.name) private UserModel: UserModelType,
-    @InjectModel(Blog.name) private BlogModel: BlogModelType,
-    @InjectModel(Post.name) private PostModel: PostModelType,
-    @InjectModel(Comment.name) private CommentModel: CommentModelType,
-    @InjectModel(Like.name) private LikeModel: LikeModelType,
+    @InjectConnection() private readonly databaseConnection: Connection,
   ) {}
 
   @Delete('all-data')
   @HttpCode(HttpStatus.NO_CONTENT)
   async deleteAllData() {
-    await this.UserModel.deleteMany({});
-    await this.BlogModel.deleteMany({});
-    await this.PostModel.deleteMany({});
-    await this.CommentModel.deleteMany({});
-    await this.LikeModel.deleteMany({});
+    const collections = await this.databaseConnection.listCollections();
+
+    const promises = collections.map((collection) =>
+      this.databaseConnection.collection(collection.name).deleteMany({}),
+    );
+    await Promise.all(promises);
+
+    return {
+      status: 'succeeded',
+    };
   }
 }
--- src/features/testing/testing.module.ts
+++ src/features/testing/testing.module.ts
@@ -1,10 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TestingController } from './testing.controller';
-import { UserAccountsModule } from '../user-accounts/user-accounts.module';
-import { BloggerPlatformModule } from '../blogger-platform/blogger-platform.module';
 
 @Module({
-  imports: [UserAccountsModule, BloggerPlatformModule],
   controllers: [TestingController],
 })
 export class TestingModule {}
